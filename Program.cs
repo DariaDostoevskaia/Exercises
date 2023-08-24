b90ad1199ceeb5d4755f910ece7302c697bfe05f
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-08-24 23:16:39 +0300

    fix: string, bool, array

--- Program.cs
+++ Program.cs
@@ -16,11 +16,11 @@
 
         private static void Main(string[] args)
         {
-            //    CalculateDivision(dividend, out int divisor);
-            //    FindMax(array, out int maxValue);
-            //    CalculateRectangleArea(width, height, out double square);
-            Swap(firstNumber, secondNumber, out int[] swapNumbers);
-            //TryParseInt(inputString, out bool result);
+            CalculateDivision(dividend, out int divisor);
+            FindMax(array, out int maxValue);
+            CalculateRectangleArea(width, height, out double square);
+            Swap(firstNumber, secondNumber, out string swapNumbers);
+            TryParseInt(inputString, out int result);
 
             void CalculateDivision(int dividend, out int divisor)
             {
@@ -28,13 +28,14 @@
                 var divisorNumber = int.Parse(Console.ReadLine());
 
                 divisor = dividendNumber / divisorNumber;
+                Console.WriteLine(divisor);
             }
 
             void FindMax(int[] array, out int maxValue)
             {
                 Console.WriteLine("Введите количество чисел: ");
                 var arrayNumber = int.Parse(Console.ReadLine());
-
+                array = new int[arrayNumber];
                 Console.WriteLine("Введите числа: ");
                 for (int i = 0; i < arrayNumber; i++)
                 {
@@ -42,6 +43,7 @@
                     array[i] = number;
                 }
                 maxValue = array.Max();
+                Console.WriteLine(maxValue);
             }
 
             void CalculateRectangleArea(double width, double height, out double square)
@@ -52,27 +54,29 @@
                 square = width * height;
             }
 
-            void Swap(int firstNumber, int secondNumber, out int[] swapNumbers)
+            void Swap(int firstNumber, int secondNumber, out string swapNumbers)
             {
                 firstNumber = int.Parse(Console.ReadLine());
                 secondNumber = int.Parse(Console.ReadLine());
 
-                (firstNumber, secondNumber) = (secondNumber, firstNumber);
-                swapNumbers = new int[] { firstNumber, secondNumber };
+                firstNumber += secondNumber;
+                secondNumber = firstNumber - secondNumber;
+                firstNumber -= secondNumber;
+                swapNumbers = firstNumber + " " + secondNumber;
+                Console.WriteLine(swapNumbers);
             }
 
-            void TryParseInt(string inputString, out bool result)
+            void TryParseInt(string inputString, out int result)
             {
-                var input = Console.ReadLine();
-                int intNumber;
+                inputString = Console.ReadLine();
 
-                if (input == null)
+                if (inputString == null)
                 {
                     Console.WriteLine("Введите значение еще раз!");
-                    TryParseInt(input, out result);
+                    TryParseInt(inputString, out result);
                 }
-                result = int.TryParse(input, out intNumber);
-                Console.WriteLine(result);
+                var resultBool = int.TryParse(inputString, out result);
+                Console.WriteLine(resultBool);
             }
         }
     }
