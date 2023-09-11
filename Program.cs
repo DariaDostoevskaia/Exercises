0353a55ad02ec70973988908ffe70d91cf3608cd
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-09-12 00:55:26 +0300

    feat: exercise 1

--- Program.cs
+++ Program.cs
@@ -1,117 +1,175 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using static Exercises.Program;
-
-namespace Exercises
+﻿namespace Exercises
 {
-    public static class Program
+    public static class OOP
     {
         private static void Main()
         {
             //Exercise 1;
 
-            var myList = new List<int> { 5543210, 273, 8, 41, 87643, 6 };
-            IEnumerable<int> evenNumbers;
-
-            GetEvenNumbers(myList);
-
-            foreach (var number in evenNumbers)
+            var persons = new List<Person>()
             {
-                Console.Write(number + " ");
-            }
-
-            void GetEvenNumbers(List<int> myList)
-            {
-                evenNumbers = myList.Where(n => n % 2 == 0);
-            }
-
-            //Exercise 2;
-
-            List<Student> students = new List<Student>(5)
-            {
-                new Student { StudentName = "John", Age = 20 },
-                new Student { StudentName = "Alice", Age = 21 },
-                new Student { StudentName = "Bob", Age = 22 },
-                new Student { StudentName = "Kate", Age = 20 },
-                new Student { StudentName = "Mike", Age = 21 }
+                new Person { PersonSurname = "Fleming", PersonName = "John", PersonAge = 20 },
+                new Person { PersonSurname = "Niumatur", PersonName = "Alice", PersonAge = 21 }
             };
 
-            IEnumerable<IGrouping<int, Student>> groupedStudents;
+            GetSurnames(persons);
+            IEnumerable<IGrouping<string, Person>> personSurnames;
 
-            GetGroupStudents(students);
+            GetNames(persons);
+            IEnumerable<IGrouping<string, Person>> personNames;
 
-            foreach (var group in groupedStudents)
+            GetAges(persons);
+            IEnumerable<IGrouping<int, Person>> ages;
+
+            GetKeys(personSurnames, personNames, ages);
+
+            void GetSurnames(List<Person> persons)
             {
-                Console.WriteLine($"Age: {group.Key}, Count: {group.Select(groupedStudents => students).Count()}");
+                personSurnames = persons.GroupBy(person => person.PersonSurname);
             }
 
-            void GetGroupStudents(IEnumerable<Student> students)
+            void GetNames(List<Person> persons)
             {
-                groupedStudents = students.GroupBy(student => student.Age);
+                personNames = persons.GroupBy(person => person.PersonName);
             }
 
-            //Exercise 3;
-
-            List<Worker> workers = new List<Worker>()
+            void GetAges(List<Person> persons)
             {
-                new Worker { WorkerName = "John", Salary = 20000 },
-                new Worker { WorkerName = "Alice", Salary = 21000 },
-                new Worker { WorkerName = "Bob", Salary = 22000 }
-            };
-
-            var groupedWorkers = new List<string>();
-
-            GetGroupWorkers(workers);
-
-            foreach (var worker in groupedWorkers)
-            {
-                Console.WriteLine(worker);
+                ages = persons.GroupBy(age => age.PersonAge);
             }
 
-            void GetGroupWorkers(List<Worker> workers)
+            void GetKeys(IEnumerable<IGrouping<string, Person>> personSurname,
+                    IEnumerable<IGrouping<string, Person>> personName,
+                      IEnumerable<IGrouping<int, Person>> age)
             {
-                groupedWorkers = workers.Select(worker => worker.WorkerName).ToList();
-            }
+                foreach (var key in personSurname)
+                {
+                    Console.WriteLine(key.Key);
+                }
+                Console.WriteLine();
 
-            //Exercise 4;
+                foreach (var key in personName)
+                {
+                    Console.WriteLine(key.Key);
+                }
+                Console.WriteLine();
 
-            var cities = new List<string>()
-            {
-            "New York",
-            "Moscow",
-            "Cheboksary",
-            "Baku",
-            "Madrid",
-            "Paris",
-            "Astana",
-            "Lisbon"
-            };
-
-            List<string> alphabeticallyCities;
-
-            GetCitiesAlphabetically(cities);
-
-            foreach (var city in alphabeticallyCities)
-                Console.WriteLine(city);
-
-            void GetCitiesAlphabetically(List<string> cities)
-            {
-                alphabeticallyCities = (cities.OrderBy(city => city).ToList());
-            }
-
-            //Exercise 5;
-
-            var numbers = new List<int> { 1, 2, 3, 4, 5 };
-            var sum = 0;
-            GetSum(numbers);
-            Console.WriteLine(sum);
-
-            void GetSum(List<int> numbers)
-            {
-                sum = numbers.Sum();
+                foreach (var key in age)
+                {
+                    Console.WriteLine(key.Key);
+                }
             }
         }
     }
+
+    //    public static class Program
+    //    {
+    //        private static void Main()
+    //        {
+    //            //Exercise 1;
+
+    //            var myList = new List<int> { 5543210, 273, 8, 41, 87643, 6 };
+    //            IEnumerable<int> evenNumbers;
+
+    //            GetEvenNumbers(myList);
+
+    //            foreach (var number in evenNumbers)
+    //            {
+    //                Console.Write(number + " ");
+    //            }
+
+    //            void GetEvenNumbers(List<int> myList)
+    //            {
+    //                evenNumbers = myList.Where(n => n % 2 == 0);
+    //            }
+
+    //            //Exercise 2;
+
+    //            List<Student> students = new List<Student>(5)
+    //        {
+    //            new Student { StudentName = "John", Age = 20 },
+    //            new Student { StudentName = "Alice", Age = 21 },
+    //            new Student { StudentName = "Bob", Age = 22 },
+    //            new Student { StudentName = "Kate", Age = 20 },
+    //            new Student { StudentName = "Mike", Age = 21 }
+    //        };
+
+    //            IEnumerable<IGrouping<int, Student>> groupedStudents;
+
+    //            GetGroupStudents(students);
+
+    //            foreach (var group in groupedStudents)
+    //            {
+    //                Console.WriteLine($"Age: {group.Key}, Count: {group.Select(groupedStudents => students).Count()}");
+    //            }
+
+    //            void GetGroupStudents(IEnumerable<Student> students)
+    //            {
+    //                groupedStudents = students.GroupBy(student => student.Age);
+    //            }
+
+    //            //Exercise 3;
+
+    //            List<Worker> workers = new List<Worker>()
+    //        {
+    //            new Worker { WorkerName = "John", Salary = 20000 },
+    //            new Worker { WorkerName = "Alice", Salary = 21000 },
+    //            new Worker { WorkerName = "Bob", Salary = 22000 }
+    //        };
+
+    //            var groupedWorkers = new List<string>();
+
+    //            GetGroupWorkers(workers);
+
+    //            foreach (var worker in groupedWorkers)
+    //            {
+    //                Console.WriteLine(worker);
+    //            }
+
+    //            void GetGroupWorkers(List<Worker> workers)
+    //            {
+    //                groupedWorkers = workers.Select(worker => worker.WorkerName).ToList();
+    //            }
+
+    //            //Exercise 4;
+
+    //            var cities = new List<string>()
+    //        {
+    //        "New York",
+    //        "Moscow",
+    //        "Cheboksary",
+    //        "Baku",
+    //        "Madrid",
+    //        "Paris",
+    //        "Astana",
+    //        "Lisbon"
+    //        };
+
+    //            List<string> alphabeticallyCities;
+
+    //            GetCitiesAlphabetically(cities);
+
+    //            foreach (var city in alphabeticallyCities)
+    //                Console.WriteLine(city);
+
+    //            void GetCitiesAlphabetically(List<string> cities)
+    //            {
+    //                alphabeticallyCities = (cities.OrderBy(city => city).ToList());
+    //            }
+
+    //            //Exercise 5;
+
+    //            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+    //            var sum = 0;
+    //            GetSum(numbers);
+    //            Console.WriteLine(sum);
+
+    //            void GetSum(List<int> numbers)
+    //            {
+    //                sum = numbers.Sum();
+    //            }
+    //        }
+    //    }
 
     public class Student
     {
@@ -125,5 +183,14 @@ namespace Exercises
         public string WorkerName { get; set; }
 
         public int Salary { get; set; }
+    }
+
+    public class Person
+    {
+        public string PersonName { get; set; }
+
+        public string PersonSurname { get; set; }
+
+        public int PersonAge { get; set; }
     }
 }
